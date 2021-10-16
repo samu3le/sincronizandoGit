769b15f0b46146217de12bdd47dc9b4d2ed860df
Nautilux24 <samuelesponline@gmail.com>
2021-10-16 17:46:00 -0500

    modificando localmente el archivo nav

--- nav.php
+++ nav.php
@@ -1,1 +1,8 @@
-
+<nav>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Story</a></li>
+    <li><a href="#">About Us</a></li>
+    <li><a href="#">Contact</a></li>
+  </ol>
+  </nav>
