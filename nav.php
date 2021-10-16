d2065923abfa09a0bd3b9ec93875e855a3b4cea8
Samuel Espinoza <samuelesponline@gmail.com>
2021-10-16 17:43:34 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,1 +1,9 @@
 
+<header>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Story</a></li>
+    <li><a href="#">About Us</a></li>
+    <li><a href="#">Contact</a></li>
+  </ol>
+  </header>
